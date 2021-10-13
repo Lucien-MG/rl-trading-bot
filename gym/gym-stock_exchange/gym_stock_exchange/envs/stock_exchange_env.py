edf2765e3bb760f2058822ef33003d3ff8655f97
Lucien Martin-Gaffé <lucien.martingaffe@gmail.com>
2021-10-13 17:54:19 +0200

    WIP: start environment creation.

--- gym/gym-stock_exchange/gym_stock_exchange/envs/stock_exchange_env.py
+++ gym/gym-stock_exchange/gym_stock_exchange/envs/stock_exchange_env.py
@@ -1,26 +1,64 @@
 #!/usr/bin/python3
 #‑∗‑ coding: utf‑8 ‑∗‑
 
+import os
+import enum
+
 import gym
+from gym import error, utils
+from gym.utils import seeding
+
+import numpy as np
 import pandas as pd
 
-from gym import error, spaces, utils
-from gym.utils import seeding
+class Actions(enum.Enum):
+    Skip = 0
+    Buy = 1
+    Close = 2
 
 class StockExchangeEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, stock_exchange_history_dir):
-        self.stock_exchange_history_dir = stock_exchange_history_dir
+    def __init__(self, stock_exchange_data_dir, state_size=10):
+        self.action_space = gym.spaces.Discrete(n=len(Actions))
+        self.observation_space = gym.spaces.Discrete(state_size)
 
-        data = pd.read_csv(self.stock_exchange_history_dir, sep=';', header=None, names=["code", "date", "opening", "max", "min", "closing", "?"])
-        print(data)
+        # Read and concat csv:
+        csv_names = os.listdir(path=stock_exchange_data_dir)
+        open_csv = [self._read_csv(os.path.join(stock_exchange_data_dir, csv_name)) for csv_name in csv_names]
+
+        # Cleaning data, removing duplicates and sorting by dates
+        self.df_stock_exchange = pd.concat(open_csv, axis=0, ignore_index=True)
+        self.df_stock_exchange.drop_duplicates(subset=['date'])
+        self.df_stock_exchange.sort_values(by=['date'])
+
+        # Define states:
+        self.commission_perc = 0.01
+        self.state_size = state_size
+        self.current_step = self.state_size
+
+    def _read_csv(self, path):
+        return pd.read_csv(path, sep=';', header=None, names=["code", "date", "opening", "max", "min", "closing", "?"])
 
     def step(self, action):
-        pass
+        assert self.action_space.contains(action)
+
+        state = self.df_stock_exchange[["opening", "max", "min", "closing"]][self.current_step - self.state_size:self.current_step].to_numpy()
+        reward = 0
+        done = self.current_step >= len(self.df_stock_exchange) - 1
+        info = {}
+
+        if action == Actions.Buy and not self.have_position:
+            reward -= reward * self.commission_perc
+        elif action == Actions.Close and self.have_position:
+            reward -= reward * self.commission_perc
+
+        self.current_step += 1
+
+        return state, reward, done, info
 
     def reset(self):
-        pass
+        self.current_step = self.state_size
 
     def render(self, mode='human'):
         pass
--- src/main.py
+++ src/main.py
@@ -4,4 +4,12 @@
 import gym
 import gym_stock_exchange
 
-env = gym.make("gym_stock_exchange:gym_stock_exchange-v0", stock_exchange_history_dir="data/cac40/2019_2021.csv")
+env = gym.make("gym_stock_exchange:gym_stock_exchange-v0", stock_exchange_data_dir="data/cac40/")
+
+env.reset()
+done = False
+
+while not done:
+    state, reward, done, info = env.step(0)
+    print(state, reward, done, info)
+
