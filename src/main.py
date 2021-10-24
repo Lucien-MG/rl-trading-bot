d6992f5bf76112aa1159a5afd8a7147c2c1ac5f7
Lucien Martin-Gaffé <lucien.martingaffe@gmail.com>
2021-10-24 15:52:05 +0200

    add class agent and random agent, also a quick fix on stock exchange env

--- src/main.py
+++ src/main.py
@@ -4,12 +4,20 @@
 import gym
 import gym_stock_exchange
 
+from agent.random import AgentRandom
+
 env = gym.make("gym_stock_exchange:gym_stock_exchange-v0", stock_exchange_data_dir="data/cac40/")
 
-env.reset()
+agent = AgentRandom(env.action_space.n)
+
+state = env.reset()
 done = False
 
 while not done:
-    state, reward, done, info = env.step(0)
+    action = agent.action(state)
+    print(action)
+    next_state, reward, done, info = env.step(action)
+
+    state = next_state
     print(state, reward, done, info)
 
