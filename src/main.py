73eb11e4f97f056bf0ac03e7df8099d8d1791816
Benjamin Decreusefond <benjamin.decreusefond@epita.fr>
2021-10-25 21:10:14 +0200

    add first graph plot

--- src/main.py
+++ src/main.py
@@ -3,8 +3,16 @@
 
 import gym
 import gym_stock_exchange
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
 
+from flask import *
 from agent.random import AgentRandom
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "random string"
 
 env = gym.make("gym_stock_exchange:gym_stock_exchange-v0", stock_exchange_data_dir="data/cac40/")
 
@@ -13,11 +21,30 @@ agent = AgentRandom(env.action_space.n)
 state = env.reset()
 done = False
 
+f = open("logs.csv", "a+")
+f.write('reward,done,cash')
+f.close()
+
+@app.route('/', methods = ["GET", "POST"])
+def index():
+    if request.method == "POST":
+        df = pd.read_csv('logs.csv', sep=",")
+        df["reward"][:500].astype(float).plot()
+        base_url = '/static/images/rewards.png'
+        plt.savefig('src' + base_url)
+        return render_template('reward.html', name='Rewards over time', url=base_url)
+    return render_template('index.html')
+
 while not done:
     action = agent.action(state)
     print(action)
     next_state, reward, done, info = env.step(action)
 
     state = next_state
+    f = open("logs.csv", "a+")
+    f.write(str(reward) + ',' + str(done) + ',' + str(info["cash"]) + '\n')
+    f.close()
     print(state, reward, done, info)
 
+if len(sys.argv) > 1 and sys.argv[1] == "--logs":
+    app.run(host='localhost', port=5000)
