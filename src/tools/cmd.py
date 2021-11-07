13083a532a950808b9365dc03254ca88de865da6
Lucien-MG <lucien.martingaffe@gmail.com>
2021-11-07 19:22:37 +0100

    add interactive mod in parsing line argument

--- src/tools/cmd.py
+++ src/tools/cmd.py
@@ -3,7 +3,7 @@ import argparse
 def argument_parser():
     parser = argparse.ArgumentParser(description='Command line controler for rltrade.')
 
-    parser.add_argument('--interactive', type=bool, default=False,
+    parser.add_argument('-i', '--interactive', action='store_true', default=False,
                     help='Activate the interactive mode in your browser.')
 
     args = parser.parse_args()
