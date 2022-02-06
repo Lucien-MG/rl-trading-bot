c1c97391ac5037adcf1e1ea34508f60bcf576756
Benjamin Decreusefond <benjamin.decreusefond@epita.fr>
2022-02-06 17:05:44 +0100

    add robustness to random agent

--- rltrade/agents/dnn/dqn_linear.py
+++ rltrade/agents/dnn/dqn_linear.py
@@ -6,6 +6,11 @@ import torch.nn as nn
 class SimpleFFDQN(nn.Module):
 
     def __init__(self, input_shape, action_space):
+        """Init DQNlinear class
+        Args:
+            input_shape (int): shape on the convolution
+            action_space (int): degree of liberty
+        """
         super(SimpleFFDQN, self).__init__()
 
         self.fc_val = nn.Sequential(
@@ -21,6 +26,10 @@ class SimpleFFDQN(nn.Module):
         )
 
     def forward(self, x):
+        """Get the linear dqn to the next step
+        Args:
+            shape (obj): the convolution
+        """
         val = self.fc_val(x)
         adv = self.fc_adv(x)
         return val + (adv - adv.mean(dim=1, keepdim=True))
--- rltrade/agents/random.py
+++ rltrade/agents/random.py
@@ -8,13 +8,33 @@ import random
 class Agent(AgentInterface):
 
     def __init__(self, config):
+        """Init the randim agent class
+        Args:
+            input_shape (file): the set up config file
+        """
         self._set_config(config)
 
     def _set_config(self, configuration):
+         """Read the config and put it in var
+        Args:
+            configuration (obj): the set up config file
+        """
         self.__dict__ = { k:v for (k,v) in configuration.__dict__.items() }
 
     def action(self, state):
+        """Decide an action
+        Args:
+            state (obj): current state of the agent
+        """
         return random.randint(0, int(self.action_space) - 1)
 
     def step(self, state, action, reward, next_state, done):
+        """Step of current state
+        Args:
+            state (obj): current state of the agent
+            action (int): the choosen action
+            reward (int): current reward
+            next_state (obj): next state env
+            done (bool): is training finished
+        """
         pass
